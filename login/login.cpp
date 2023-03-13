85c8bcf5546e0623542bce4e0a8b91acba63fa61
zRain204 <126950997+zRain204@users.noreply.github.com>
2023-03-13 23:15:33 +0700

    struct and function prototype

--- login/login.cpp
+++ login/login.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <string.h>
 #include <fstream>
 using namespace std;
 
@@ -7,25 +8,25 @@ struct account {
     char* password;
 };
 
-
 void input1Account(account& data) 
 {
-    data.userName = new char[20];
-    cin.get(data.userName, 20, '\n');
+    data.userName = new char[21];
+    cin.get(data.userName, 21, '\n');
     cin.ignore(100, '\n');
-    data.password = new char[20];
-    cin.get(data.password, 20, '\n');
+    data.password = new char[21];
+    cin.get(data.password, 21, '\n');
 }
 
 void input1Account(account& data, ifstream& fin) 
 {
-    data.userName = new char[20];
-    fin.get(data.userName, 20, '\n');
+    data.userName = new char[21];
+    fin.get(data.userName, 21, '\n');
     fin.ignore(100, '\n');
-    data.password = new char[20];
-    fin.get(data.password, 20, '\n');
+    data.password = new char[21];
+    fin.get(data.password, 21, '\n');
 }
 
+// if 1st username is null -> crash
 void inputAccounts(account* login_data, int& numberOfAccount, ifstream& fin) 
 {
     numberOfAccount = 0;
@@ -36,7 +37,7 @@ void inputAccounts(account* login_data, int& numberOfAccount, ifstream& fin)
     }
 }
 
-bool checkLoginData(account data1, account data2) 
+bool isDataTheSame(account data1, account data2) 
 {
     bool checkUserName = false;
     if (strcmp(data1.userName, data2.userName) == 0)
@@ -54,8 +55,11 @@ bool checkLoginData(account data1, account data2)
 
 bool login(account* login_data, int n, account inputLoginData)
 {
+    if (inputLoginData.userName == (char*)'\0' || inputLoginData.password == (char*)'\0') {
+        return false;
+    }
     for (int i = 0; i < n; i++)
-        if (checkLoginData(login_data[i], inputLoginData))
+        if (isDataTheSame(login_data[i], inputLoginData))
             return true;
     return false;
 }
@@ -67,10 +71,17 @@ void addinfo(account person, char* filename, ofstream& fout)
     fout << person.password << endl;
 }
 
+bool isPasswordthesame(char* pass1, char* pass2) {
+    if (strcmp(pass1, pass2) == 0)
+        return true;
+    else
+        return false;
+}
+
 int main()
 {
     ifstream fin;
-    fin.open("input.txt");
+    fin.open("input1.txt");
     account* login_data = new account[100];
     int n;
     inputAccounts(login_data, n, fin);
@@ -91,7 +102,7 @@ int main()
     input1Account(newinfo);
 
     ofstream fout;
-    addinfo(newinfo, (char*)"input.txt", fout);
+    addinfo(newinfo, (char*)"input1.txt", fout);
     fout.close();
     delete[] login_data;
     return 0;
---
+++ struct and function prototype/struct and function prototype.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+struct scoreboard {
+    double totalMark;
+    double finalMark;
+    double midtermMark;
+    double otherMark;
+};
+
+struct person {
+    char* firstName;
+    char* lastName;
+    bool gender; // 0(false) : male; 1(true) : female
+    Date dob;
+    char* socialID;
+};
+
+struct student {
+    int No;
+    char stuID[9];
+    person student;
+    scoreboard mark;
+};
+
+struct staff {
+    char* headTeacher; // if staff is head teacher -> save the class he/she teaches, otherwise none
+    char* department; // Ex: khoa CNTT, khoa giao vu, khoa luat...
+    char* staffID;
+    person staff;
+};
+
+struct course {
+    char* courseID;
+    char* courseName;
+    char* className;
+    char* teacherName;
+    int numOfCre;
+    int maxStu = 50;
+    char dayofweek[4];
+    char sessionHour[3]; // S1 (07:30), S2 (09:30), S3(13:30) and S4 (15:30)).
+    // A course will be taught in only one session in a week
+    student* stuOfCourse;
+};
+
+struct semester {
+    char schoolYear[10];
+    int No;
+    Date startDate;
+    Date endDate;
+    int numOfCourse;
+    course* courseName;
+};
+
+struct Class {
+    char* classID;
+    int numOfStu;
+    student* stuInClass;
+};
+
+// At the beginning of a school year (often in September), an academic staff member will:
+void addSchoolYear(int schoolYear, char* filename, ofstream* fout); // 1
+// add new school to text file.
+
+void createAClass(Class& className, char* filename, ofstream* fout); // 2
+
+void addAStudentToClass(student newStu, char* filename, ofstream* fout); // 3
+void addStudentToClass(student* newStudents, char* filename, ofstream* fout); // 4
+
+// At the beginning of a semester, an academic staff member will:
+void CreateASemester(semester& newSemester, char* filename, ofstream* fout); // 6
+void AddACourse(); // 7
+void addStudentsToCourse(); // 8
+void viewCourses(); // 9
+void updateCourseInformation(); // 10
+void removeStudentToCourse(); // 11
+void addAStudentToCourse(); // 12
+void DeleteACourse(); // 13
+
+// In a semester, a student still can:
+void viewStudingCourses(); // 14
+
+// At any time, an academic staff member can:
+void viewClasses(); // 15
+void viewStudentsInAClass(); // 16
+void viewCourses(); // 17
+void viewStudentInACourse(); // 18
+
+// At the end of a semester, an academic staff member can:
+void exportStudentsInACourse(); // 19
+void importScoreboardOfACourse(); // 20
+void viewScoreboardOfACourse(); // 21
+void updateStudentResult(); // 22
+void importScoreboardOfAClass(); // 23
+
+// When the scoreboard has been published (by the academic staff member), a student can:
+void viewScoreboard(); // 24
+
+int main()
+{
+    return 0;
+}
