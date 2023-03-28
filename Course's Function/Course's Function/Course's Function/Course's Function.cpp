b5da3025da646ea4cd6fda22acf43b63bb084228
duxgg <dug26062004@gmail.com>
2023-03-29 00:18:38 +0700

    Update Course's Function.cpp

--- Course's Function/Course's Function/Course's Function/Course's Function.cpp
+++ Course's Function/Course's Function/Course's Function/Course's Function.cpp
@@ -110,11 +110,7 @@ void course:: Add_Course( char* semeterfile, ofstream& fout) {
 		i++;
 	}
 }*/
-void overall_page() {
-
-}
-int main() {
-
+void viewcourse() {
 	course  course[10];
 	float Pos = 00;
 	int scrollSpeed = 35;
@@ -123,58 +119,59 @@ int main() {
 	char str[6] = "";
 	button_up = LoadTexture("but.png");
 	button_down = LoadTexture("tub.png");
-	float Yrec = button_up.height+Pos/1500*920;
-	float recheight = (920 - 2 * button_up.height) * 920 / (1500+920);
+	float Yrec = button_up.height + Pos / 1500 * 920;
+	float recheight = (920 - 2 * button_up.height) * 920 / (1500 + 920);
 	float tmp = 0;
 	int check = 0;
 	while (!WindowShouldClose()) {
-		Pos += ((GetMouseWheelMove() * scrollSpeed) - (IsKeyDown(KEY_DOWN) - IsKeyDown(KEY_UP)));//Mouse wheel + key up down|| chua co scroll bar
-		Yrec = button_up.height - float(Pos*(920-2*button_up.height)/(1500+920));
+		Pos += ((GetMouseWheelMove() * scrollSpeed) - (IsKeyDown(KEY_DOWN) - IsKeyDown(KEY_UP)));
+		Yrec = button_up.height - float(Pos * (920 - 2 * button_up.height) / (1500 + 920));
 		if (Pos > 0)Pos = 0;
 		if (Pos < -1500)Pos = -1500;//num of course/6*wight
 		BeginDrawing();
 		ClearBackground(SKYBLUE);
-		DrawTexture(button_up,1590,0,WHITE);
-		DrawTexture(button_down, 1590,890, WHITE);
-		if (CheckCollisionPointRec(GetMousePosition(), { 1590, 0, float(button_up.width),float(button_up.height)})) {
+		DrawTexture(button_up, 1590, 0, WHITE);
+		DrawTexture(button_down, 1590, 890, WHITE);
+		if (CheckCollisionPointRec(GetMousePosition(), { 1590, 0, float(button_up.width),float(button_up.height) })) {
 			DrawTexture(button_up, 1590, 0, LIGHTGRAY);
-				if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-				DrawTexture(button_up, 1590, 0,GRAY);
+			if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+				DrawTexture(button_up, 1590, 0, GRAY);
 				Pos += 1;
 			}
 		}
-		if (CheckCollisionPointRec(GetMousePosition(), {1590,890,float(button_down.width),float(button_down.height)})) {
+		if (CheckCollisionPointRec(GetMousePosition(), { 1590,890,float(button_down.width),float(button_down.height) })) {
 			DrawTexture(button_down, 1590, 890, LIGHTGRAY);
 			if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
 				DrawTexture(button_down, 1590, 890, GRAY);
 				Pos -= 1;
 			}
 		}
-		if (!CheckCollisionPointRec(GetMousePosition(), { 1590,Yrec,float(button_down.width),recheight }))DrawRectangle(1590, Yrec,button_up.width,recheight, RAYWHITE);
+		if (!CheckCollisionPointRec(GetMousePosition(), { 1590/*screnwidt*/,Yrec,float(button_down.width),recheight}))DrawRectangle(1590, Yrec, button_up.width, recheight, RAYWHITE);
 		if (CheckCollisionPointRec(GetMousePosition(), { 1590,Yrec,float(button_down.width),recheight }) && IsMouseButtonUp(MOUSE_BUTTON_LEFT)) {
 			DrawRectangle(1590, Yrec, button_up.width, recheight, LIGHTGRAY);
 			tmp = Yrec - GetMouseY();
 		}
 		if (CheckCollisionPointRec(GetMousePosition(), { 1590,Yrec,float(button_down.width),recheight }) && IsMouseButtonDown(MOUSE_BUTTON_LEFT))check = 1;
 		if (IsMouseButtonUp(MOUSE_BUTTON_LEFT))check = 0;
-		if (check==1) {
-			Yrec = tmp+GetMouseY();
-			Pos = (button_up.height - Yrec)*(1500 + 920)/ (920 - 2 * button_up.height);
+		if (check == 1) {
+			Yrec = tmp + GetMouseY();
+			Pos = (button_up.height - Yrec) * (1500 + 920) / (920 - 2 * button_up.height);
 			if (Pos > 0) { Pos = 0; Yrec = button_up.height - float(Pos * (920 - 2 * button_up.height) / (1500 + 920)); }
 			if (Pos < -1500) { Pos = -1500; Yrec = button_up.height - float(Pos * (920 - 2 * button_up.height) / (1500 + 920)); }
 			DrawRectangle(1590, Yrec, button_up.width, recheight, GRAY);
 		}
-		
+
 		int i = 0;//k=num of courses
 		for (int k = 0; k < 10; k++) {
-			DrawText(TextFormat("Course: " ), 17, Pos + (i += 20), 20,MAROON);
-			DrawText(str,100, Pos+(i), 20, MAROON);
+			if (CheckCollisionPointRec(GetMousePosition(), { 10,Pos + i + 10,300, 140 })) {
+				DrawRectangleLines(10, Pos + i + 10, 300, 140, BLACK);
+				if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))return;//->view into 1 course
+			}
+			else DrawRectangleLines(10,Pos+i+10,300, 140,WHITE);
+			DrawText(TextFormat("Course: "), 17, Pos + (i += 20), 20, MAROON);
 			DrawText(TextFormat("ID: "), 17, Pos + (i += 20), 20, MAROON);
-			DrawText(str, 50, Pos + (i), 20, MAROON);
 			DrawText(TextFormat("Teacher: ", course[k].teacherName), 17, Pos + (i += 20), 20, MAROON);
-			DrawText(str, 115, Pos + (i), 20, MAROON);
 			DrawText(TextFormat("Number of Credit: ", course[k].numOfCre), 17, Pos + (i += 20), 20, MAROON);
-			DrawText(str, 205, Pos + (i), 20, MAROON);
 			DrawText(TextFormat("Course Day: ", course[k].dayofweek), 17, Pos + (i += 20), 20, MAROON);
 			DrawText(TextFormat("Session Hours: ", course[k].dayofweek), 17, Pos + (i += 20), 20, MAROON);
 			i += 50;
@@ -182,4 +179,7 @@ int main() {
 		EndDrawing();
 	}
 	CloseWindow();
+}
+int main() {
+	
 }
