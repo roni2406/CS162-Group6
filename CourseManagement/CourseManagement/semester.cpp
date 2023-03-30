1d845211bee53c00eb6255d0e88716b3fbbeb50f
zRain204 <126950997+zRain204@users.noreply.github.com>
2023-03-30 12:47:40 +0700

    more semester functions

--- CourseManagement/CourseManagement/semester.cpp
+++ CourseManagement/CourseManagement/semester.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <direct.h>
 #include <fstream>
 #include "Date.h"
 #include "schoolYear.h"
@@ -25,18 +26,29 @@ void semester::inputASemesterWithCSVFile(ifstream& fin) {
 	delete[] tmp;
 }
 
-void semester::outputASemesterToCSVFile(char* filename) {
+void semester::outputASemesterToCSVFile(char* school_year) {
 	ifstream fin;
-	fin.open(filename);
+	fin.open("../data/" + (string)(school_year) + "semester.csv");
 	int numCourse = 0;
+	fin.get();
 	while (!fin.eof()) {
 		++numCourse;
 		fin.ignore(500, '\n');
 	}
 	No = numCourse + 1;
+	switch (numCourse) {
+	case 0:
+		if (_mkdir(("../data/" + (string)(school_year) + "fall").c_str()));
+		break;
+	case 1:
+		if (_mkdir(("../data/" + (string)(school_year) + "summer").c_str()));
+		break;
+	case 2:
+		if (_mkdir(("../data/" + (string)(school_year) + "autumn").c_str()));
+	}
 	fin.close();
 	ofstream fout;
-	fout.open(filename, ios::app);
+	fout.open(school_year, ios::app);
 	if(numCourse != 0) fout << endl;
 	fout << No << ",";
 	startDate.outputADateToFile(fout);
@@ -49,10 +61,30 @@ void semester::outputASemesterToCSVFile(char* filename) {
 bool createASemester(char* school_year, char* start_date, char* end_date, int number_of_course) {
 	semester s;
 	s.inputASemester(school_year, start_date, end_date, number_of_course);
-	char* filename = new char[100];
-	filename = _strdup(school_year);
-	strcat_s(filename, strlen(filename) + 13, (char*)"semester.csv");
-	s.outputASemesterToCSVFile(filename);
-	delete[] filename;
+	s.outputASemesterToCSVFile(school_year);
 	return true;
+}
+
+int countSemester(char* school_year) {
+	ifstream fin;
+	fin.open("../data/" + (string)(school_year)+"semester.csv");
+	int numCourse = 0;
+	fin.get();
+	while (!fin.eof()) {
+		++numCourse;
+		fin.ignore(500, '\n');
+	}
+	return numCourse;
+}
+
+semester* getSemester(char* school_year) {
+	int numOfSemester = countSemester(school_year);
+	semester* semesterArr = new semester[5];
+	ifstream fin;
+	fin.open("../data/" + (string)(school_year)+"semester.csv");
+	for (int i = 0; i < numOfSemester; i++) {
+		semesterArr[i].inputASemesterWithCSVFile(fin);
+	}
+	fin.close();
+	return semesterArr;
 }
