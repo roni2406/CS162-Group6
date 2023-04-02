a80ad03f25572da8d57d732349ef6677f7d4312e
duxgg <dug26062004@gmail.com>
2023-04-02 09:08:16 +0700

    add course

--- CourseManagement/CourseManagement/course.h
+++ CourseManagement/CourseManagement/course.h
@@ -1,21 +1,71 @@
 #include <iostream>
 #include "users.h"
+#include"Function.h"
 using namespace std;
+
 
 #ifndef COURSE_H
 #define COURSE_H
 
-struct course {
-    char* courseID;
-    char* courseName;
-    char* className;
-    char* teacherName;
-    int numOfCre;
-    int maxStu = 50;
-    char dayofweek[4];
-    char sessionHour[3]; // S1 (07:30), S2 (09:30), S3(13:30) and S4 (15:30)).
-    // A course will be taught in only one session in a week
-    student* stuOfCourse;
+struct Date {
+    int day;
+    int month;
+    int year;
 };
 
+struct scoreboard {
+    double totalMark;
+    double finalMark;
+    double midtermMark;
+    double otherMark;
+};
+
+struct person {
+    char firstName[10];
+    char lastName[20];
+    bool gender; // 0(false) : male; 1(true) : female
+    Date dob;
+    char socialID[20];
+};
+
+struct student {
+    int No;
+    char stuID[9];
+    person person;
+    scoreboard mark;
+};
+
+struct course {
+    char courseID[30];
+    char courseName[30];
+    char className[30];
+    char teacherName[30];
+    char numOfCre[30];
+    int numOfStu = 0;
+    int maxStu = 50;
+    char dayofweek[30];
+    char sessionHour[30]; // S1 (07:30), S2 (09:30), S3(13:30) and S4 (15:30)).
+    // A course will be taught in only one session in a week
+    char studentfile[30];
+    student stuOfCourse[50];
+    void Load_stu(char* filename);
+
+};
+
+struct ListCourse {
+
+    course course[50];
+    int len = 0;
+};
+
+void delete_stu(ListCourse& list, int k, int no);
+void delete_course(ListCourse& list, int i);
+void Stu_to_file(ListCourse& list, int k);
+void Course_to_File(ListCourse& list);
+void Load_Course(ListCourse& list);
+void Stus_to_Course(ListCourse& list);
+void viewcourse(ListCourse& list);
+void view_Stu(ListCourse& list, int k);
+void Input_course(ListCourse& list);
+void Stus_to_Course(ListCourse& list);
 #endif
